4e4896a0e334910698c69f7eb1159ee98a1f2c96
Chris Haumesser <1550854+wryfi@users.noreply.github.com>
2018-04-25 06:33:46 -0700

    working mvp

--- README.md
+++ README.md
@@ -3,5 +3,142 @@
 This is a reimplementation of [cmur2/dyndnsd](https://github.com/cmur2/dyndnsd),
 which is a ruby implementation of the DynDNS dynamic DNS update API.
 
-I didn't want to tolerate Ruby's baggage on my DNS server, so I decided
-to write this Go implementation.
+## purpose
+
+I didn't want to haul Ruby's baggage onto my DNS server, so I reimplemented
+the idea in go. This implementation is entirely designed around my
+personal use case, which is generating a zone for
+[NSD](https://www.nlnetlabs.nl/projects/nsd/about/).
+
+Most router firmwares support the DynDNS API implemented here, so that
+when your ISP changes your dynamic external IP address, your router can
+inform dyndnsd, which can update DNS records accordingly.
+
+## installation
+
+To install this implementation of `dyndnsd`, all you
+need to do is grab the latest binary from the GitHub Releases page,
+and put it on your system's path.
+
+## configuration
+
+The configuration is designed to be largely compatible with that of
+cmur2/dyndnsd, with a couple of notable changes:
+
+* There is (currently) only one updater, and it is called `zonefile` (you
+may have used the value `command_with_bind_zone` with cmur2/dyndnsd).
+* There is no `db` setting in this implementation of `dyndnsd`. In this
+implementation, everything is read directly from the zone file.
+* There is no `additional_zone_content` setting recognized in this
+implementation. If you want additional zone content, you can edit the
+zone file directly, and add it yourself. `dyndnsd` will preserve any
+valid records that it finds there.
+* `ttl` (and all other similar settings) must be expressed as an integer
+in seconds (`5m` is not a valid setting; use `300` instead).
+* `users` configuration is not parsed, as users are not currently
+supported.
+* `updater.params.serial_incrementer` sets the method for incrementing the
+zone file serial number. Valid options are `epoch_s` to use a UNIX epoch
+timestamp as the serial number, or `iso8601` to use a date-based format.
+* Several additional options are added for use when dyndnsd has to create
+a zone file from scratch.
+
+### configuration file locations
+
+dyndnsd will look for its configuration in:
+
+* $HOME/.config/dyndnsd/config.yml
+* /etc/dyndnsd/config.yml
+
+### example configuration
+
+```
+host: 127.0.0.1
+port: 8245
+domain: "zyx.zig.zag"
+updater:
+  name: "zonefile"
+  params:
+    # make sure to register zone file in your nsd.conf
+    zone_file: "/etc/dyndnsd/zones/zyx.zig.zag.zone"
+    # fake DNS update (discards NSD stats)
+    command: "nsd-control reload zyx.zig.zag"
+    dns: "ns.zig.zag."
+    email_addr: "hostmaster.zig.zag."
+    serial_incrementer: "epoch_s"
+    ttl: 300         # used for updated and newly created A and AAAA records
+    refresh: 900     # only used when creating a new zone file
+    retry: 300       # only used when creating a new zone file
+    expire: 86400    # only used when creating a new zone file
+    negttl: 900      # only used when creating a new zone file
+    soattl: 1800     # only used when creating a new zone file
+    nsttl: 1800      # only used when creating a new zone file
+```
+
+### serial incrementers
+
+There are two zone serial number incrementers implemented:
+
+1. `epoch_s` - the default, this setting always uses the current UNIX
+epoch timestamp as the serial number. It does no further checking of
+whether the serial number is going forward, backward, or otherwise.
+1. `iso8601` - this uses a string consisting of the iso8601 date, plus
+a two-digit incrementer, e.g. `2018042000` for the first serial number
+on April 20, 2018. This incrementer only works up until the final two
+digits hit 99, then it stops incrementing.
+
+## modus operandi
+
+`dyndnsd` listens on the configured port for HTTP GET requests at the
+URL `/nic/update`. As part of a valid request, it expects to receive URL
+parameters `hostname`, `myip`, and/or `myip6`. `hostname` is mandatory,
+but `myip` and `myip6` are optional. A complete request using curl might
+look like:
+
+```
+curl 'http://localhost:8245/nic/update?hostname=ziggy.zyx.zig.zag&myip=192.123.230.41&myip6=2003:0db8:85a3:0000:0000:8a2e:0370:7344'
+```
+
+The above request will cause dyndnsd to read its configured zone file.
+If there is an existing A record matching `ziggy.zyx.zig.zag`, it will
+be updated to point to `192.123.239.41`. Likewise, if there is an existing
+AAAA record, it will be updated. If either record does not exist, it will
+be added accordingly.
+
+If both `myip` and `myip6` are omitted from the request, `dyndnsd` will
+register the IP address from the `X-Forwarded-For` header (if set) or
+from the request IP (if not).
+
+The zone file will be updated accordingly, and its serial number will be
+incremented.
+
+Finally, the command specified in the configuration section
+`updater.params.command` will be run.
+
+
+### data preservation
+
+As much as possible `dyndnsd` will preserve whatever data it finds in its
+zone file. It will only update the SOA record serial number, and any
+matching A or AAAA records. All other valid records are preserved as-is.
+It should be safe, therefore, to add whatever custom records you want
+directly to the zone file. As long as they are not A or AAAA records
+that match incoming requests, they should pass through unaffected.
+
+`dyndnsd` does not routinely update SOA fields except for the serial
+number. The SOA-related settings in the configuration file are for
+occasions when no zone file exists, and `dyndnsd` must generate a
+valid zone file from scratch.
+
+## missing features
+
+Compared to cmur2/dyndnsd, this implementation is missing at least the
+following features:
+
+* Users and access controls
+
+## deployment
+
+It is recommended to run `dyndnsd` so that it listens only on `localhost`
+(the default is `localhost:8245`), and then use a reverse proxy like
+nginx or caddy to add security, like https and basic auth, in front of it.
--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"path/filepath"
 	"fmt"
+	"net/http"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/miekg/dns"
 	"github.com/mitchellh/go-homedir"
+	"github.com/codeskyblue/go-sh"
 	"github.com/spf13/viper"
 )
 
@@ -25,6 +29,19 @@ func main() {
 	viper.AddConfigPath("/etc/dyndnsd")
 	viper.SetDefault("host", "127.0.0.1")
 	viper.SetDefault("port", "8245")
+	viper.SetDefault("domain", "zyx.zig.zag")
+	viper.SetDefault("updater.name", "zonefile")
+	viper.SetDefault("updater.params.zone_file", "zyx.zig.zag.zone")
+	viper.SetDefault("updater.params.dns", "ns.zig.zag.")
+	viper.SetDefault("updater.params.email_addr", "hostmaster.zig.zag.")
+	viper.SetDefault("updater.params.serial_incrementer", "epoch_s")
+	viper.SetDefault("updater.params.ttl", 300)
+	viper.SetDefault("updater.params.refresh", 900)
+	viper.SetDefault("updater.params.retry", 300)
+	viper.SetDefault("updater.params.expire", 86400)
+	viper.SetDefault("updater.params.negttl", 900)
+	viper.SetDefault("updater.params.soattl", 1800)
+	viper.SetDefault("updater.params.nsttl", 1800)
 	err = viper.ReadInConfig()
 	if err != nil {
 		log.Fatalf("Fatal error with config file: %s \n", err)
@@ -57,10 +74,11 @@ func updateHandler(response http.ResponseWriter, request *http.Request) {
 				}
 			}
 		}
-		log.Println(requestParams)
 		if viper.GetString("updater.name") == "zonefile" {
 			log.Println("updating zonefile")
-			updateZoneFile(requestParams)
+			zonefileUpdater(requestParams)
+		} else {
+			log.Println("no supported updater was set in config")
 		}
 	} else {
 		log.Println("empty parameters")
@@ -74,6 +92,9 @@ func processUrlParams(response http.ResponseWriter, request *http.Request) map[s
 	hostnames, hostnameOk := request.URL.Query()["hostname"]
 	if hostnameOk && len(hostnames) == 1 {
 		if strings.HasSuffix(hostnames[0], domain) && hostnames[0] != domain {
+			if !strings.HasSuffix(hostnames[0], ".") {
+				hostnames[0] = fmt.Sprintf("%s.", hostnames[0])
+			}
 			params["hostname"] = hostnames[0]
 		}
 	}
@@ -97,22 +118,168 @@ func processUrlParams(response http.ResponseWriter, request *http.Request) map[s
 	return params
 }
 
-func updateZoneFile(params map[string]string) {
+
+func zonefileUpdater(params map[string]string) {
 	zonefile := viper.GetString("updater.params.zone_file")
 	domain := viper.GetString("domain")
 	if _, err := os.Stat(zonefile); err == nil {
-		log.Printf("found zonefile %s", zonefile)
-		zfile, err := os.Open(zonefile)
-		defer zfile.Close()
-		if err == nil {
-			log.Println("parsing zonefile")
-			for token := range dns.ParseZone(zfile, domain, zonefile) {
-				if token.Error != nil {
-					log.Println(token.Error)
-				} else {
-					fmt.Println(token.RR)
-				}
+		log.Printf("found zone file: %s", zonefile)
+		updateExistingZone(zonefile, domain, params)
+	} else {
+		log.Printf("creating new zone file: %s", zonefile)
+		createNewZone(zonefile, domain, params)
+	}
+	command := viper.GetString("updater.params.command")
+	if len(command) > 0 {
+		fields := strings.Fields(command)
+		out, err := sh.Command(fields[0], fields[1:]).Output()
+		if err != nil {
+			log.Println(err)
+		} else {
+			fmt.Println(string(out))
+		}
+	}
+}
+
+
+func updateExistingZone(zonefile string, domain string, params map[string]string) {
+	var records []dns.RR
+	var newRecords []dns.RR
+	foundA := false
+	found6A := false
+	zoneopener, err := os.Open(zonefile)
+	if err == nil {
+		log.Println("parsing zone file")
+		for token := range dns.ParseZone(zoneopener, domain, zonefile) {
+			if token.Error != nil {
+				log.Println(token.Error)
+			} else {
+				records = append(records, token.RR)
 			}
 		}
 	}
+	zoneopener.Close()
+	for _, record := range records {
+		header := record.Header()
+		if header.Rrtype == dns.TypeSOA {
+			typeRecord := record.(*dns.SOA)
+			typeRecord.Serial = incrementSerial(typeRecord.Serial)
+			newRecords = append(newRecords, record)
+		} else if header.Rrtype == dns.TypeA && header.Name == params["hostname"] {
+			foundA = true
+			if _, ok := params["ip4"]; ok {
+				typeRecord := record.(*dns.A)
+				typeRecord.A = net.ParseIP(params["ip4"])
+			}
+			newRecords = append(newRecords, record)
+		} else if header.Rrtype == dns.TypeAAAA && header.Name == params["hostname"] {
+			found6A = true
+			if _, ok := params["ip6"]; ok {
+				typeRecord := record.(*dns.AAAA)
+				typeRecord.AAAA = net.ParseIP(params["ip6"])
+			}
+			newRecords = append(newRecords, record)
+		} else {
+			newRecords = append(newRecords, record)
+		}
+	}
+	if !foundA {
+		if _, ok := params["ip4"]; ok {
+			log.Println("adding new A record")
+			ttl := viper.GetInt("updater.params.ttl")
+			record := new(dns.A)
+			record.Hdr = dns.RR_Header{Name: params["hostname"], Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(ttl)}
+			record.A = net.ParseIP(params["ip4"])
+			newRecords = append(newRecords, record)
+		}
+	}
+	if !found6A {
+		if _, ok := params["ip6"]; ok {
+			log.Println("adding new AAAA record")
+			ttl := viper.GetInt("updater.params.ttl")
+			record := new(dns.AAAA)
+			record.Hdr = dns.RR_Header{Name: params["hostname"], Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: uint32(ttl)}
+			record.AAAA = net.ParseIP(params["ip6"])
+			newRecords = append(newRecords, record)
+		}
+	}
+	zonewriter, _ := os.Create(zonefile)
+	defer zonewriter.Close()
+	writer := bufio.NewWriter(zonewriter)
+	for _, record := range newRecords {
+		fmt.Fprintln(writer, record)
+	}
+	writer.Flush()
+}
+
+
+func incrementSerial(serial uint32) uint32 {
+	incrementer := viper.GetString("updater.params.serial_incrementer")
+	if incrementer == "epoch_s" {
+		serial := time.Now().Unix()
+		return uint32(serial)
+	} else if incrementer == "iso8601" {
+		// serial is a uint32; convert to string
+		serialStr := strconv.Itoa(int(serial))
+		today := time.Now().UTC().Format("20060102")
+		// if the existing serial starts with today's date, increment it by 1;
+		// otherwise append 00 to today's date, and set that as the new serial
+		if strings.HasPrefix(serialStr, today) {
+			if !strings.HasSuffix(serialStr, "99") {
+				return serial + 1
+			} else {
+				log.Println("cannot increment iso8601 serial past 99")
+				// TODO: return an err
+				return serial
+			}
+		} else {
+			newSerial, _ := strconv.Atoi(fmt.Sprintf("%s00", today))
+			return uint32(newSerial)
+		}
+	}
+	return serial
+}
+
+func createNewZone(zonefile string, domain string, params map[string]string) {
+	var records []dns.RR
+	soattl := viper.GetInt("updater.params.soattl")
+	soa := new(dns.SOA)
+	soa.Hdr = dns.RR_Header{Name: fmt.Sprintf("%s.", domain), Rrtype: dns.TypeSOA, Class: dns.ClassINET, Ttl: uint32(soattl)}
+	soa.Refresh = uint32(viper.GetInt("updater.params.refresh"))
+	soa.Retry = uint32(viper.GetInt("updater.params.retry"))
+	soa.Expire = uint32(viper.GetInt("updater.params.expire"))
+	soa.Minttl = uint32(viper.GetInt("updater.params.negttl"))
+	soa.Mbox = viper.GetString("updater.params.email_addr")
+	soa.Ns = viper.GetString("updater.params.dns")
+	soa.Serial = incrementSerial(0)
+	records = append(records, soa)
+
+	nsttl := viper.GetInt("updater.params.nsttl")
+	ns := new(dns.NS)
+	ns.Hdr = dns.RR_Header{Name: fmt.Sprintf("%s.", domain), Rrtype: dns.TypeNS, Class: dns.ClassINET, Ttl: uint32(nsttl)}
+	ns.Ns = viper.GetString("updater.params.dns")
+	records = append(records, ns)
+
+	ttl := viper.GetInt("updater.params.ttl")
+	if _, ok := params["ip4"]; ok {
+		record := new(dns.A)
+		record.Hdr = dns.RR_Header{Name: params["hostname"], Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(ttl)}
+		record.A = net.ParseIP(params["ip4"])
+		records = append(records, record)
+	}
+
+	if _, ok := params["ip6"]; ok {
+		record := new(dns.AAAA)
+		record.Hdr = dns.RR_Header{Name: params["hostname"], Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: uint32(ttl)}
+		record.AAAA = net.ParseIP(params["ip6"])
+		records = append(records, record)
+	}
+
+	zoneopener, _ := os.Create(zonefile)
+	defer zoneopener.Close()
+	writer := bufio.NewWriter(zoneopener)
+	for _, record := range records {
+		fmt.Fprintln(writer, record)
+	}
+	writer.Flush()
 }
